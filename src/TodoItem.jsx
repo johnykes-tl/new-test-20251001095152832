c5585ac33c947b1bc229f6c04bcedb732d371129
johnykes-tl <ioan@titanlabs.tech>
2025-10-01 12:51:56 +0300

    1759312314815

---
+++ src/TodoItem.jsx
@@ -0,0 +1,10 @@
+function TodoItem({ todo, deleteTodo, toggleTodo }) {
+  return (
+    <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+      <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </li>
+  );
+}
+
+export default TodoItem;
---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, deleteTodo, toggleTodo }) {
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
